28aefa6494681eff6bc1a4ccbba008209bc7d194
hiren pandit <hirenppandit@gmail.com>
2021-03-27 23:27:47 +0530

    set base path for greeting controller

--- src/main/java/com/example/demospringsecurity/GreetingController.java
+++ src/main/java/com/example/demospringsecurity/GreetingController.java
@@ -3,18 +3,20 @@ package com.example.demospringsecurity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/greeting")
 public class GreetingController {
 
-    @GetMapping("/greeting/admin/{name}")
+    @GetMapping("/admin/{name}")
     public ResponseEntity<String> greetAdmin(@PathVariable String name) {
         return ResponseEntity.ok("Greeting Admin, "+name);
     }
 
 
-    @GetMapping("/greeting/user/{name}")
+    @GetMapping("/user/{name}")
     public ResponseEntity<String> greetUser(@PathVariable String name) {
         return ResponseEntity.ok("Greeting User, "+name);
     }
