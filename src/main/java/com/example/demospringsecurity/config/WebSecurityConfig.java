f82caf52a06fda9fa4575399baa8bb38eac92ee1
hiren pandit <hirenppandit@gmail.com>
2021-03-27 13:46:01 +0530

    revert to simple basic authentication

--- src/main/java/com/example/demospringsecurity/config/WebSecurityConfig.java
+++ src/main/java/com/example/demospringsecurity/config/WebSecurityConfig.java
@@ -1,23 +1,16 @@
 package com.example.demospringsecurity.config;
 
-import org.aopalliance.intercept.MethodInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.*;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 
 import java.util.function.Function;
 
@@ -33,30 +26,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        PreAuthHeaderTokenFilter filter = new PreAuthHeaderTokenFilter("User-ID");
-        filter.setAuthenticationManager(new AuthenticationManager()
-        {
-            @Override
-            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-                String principal = (String) authentication.getPrincipal();
-                if (!"User-ID".equals(principal)) {
-                    throw new BadCredentialsException("The API key was not found or not the expected value.");
-                }
-                authentication.setAuthenticated(true);
-                return authentication;
-            }
-        });
-
         http.authorizeRequests()
                 .antMatchers("/actuator/*")
                 .permitAll()
-                .and()
-                .addFilter(filter)
-            .authorizeRequests()
                 .anyRequest()
                 .authenticated()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .httpBasic();
     }
@@ -76,28 +50,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .build();
         return new InMemoryUserDetailsManager(userDetails);
     }
-
-
-    //Beans for pre-authentication
-
-//    @Bean
-//    @Override
-//    public AuthenticationManager authenticationManager() {
-//        return new ProviderManager(preauthProvider());
-//    }
-//
-//    @Bean
-//    PreAuthenticatedAuthenticationProvider preauthProvider() {
-//        PreAuthenticatedAuthenticationProvider authenticationProvider = new PreAuthenticatedAuthenticationProvider();
-//        authenticationProvider.setPreAuthenticatedUserDetailsService(userDetailsServiceWrapper());
-//        return authenticationProvider;
-//    }
-//
-//    private UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> userDetailsServiceWrapper() {
-//        UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> userDetails = new UserDetailsByNameServiceWrapper();
-//        userDetails.setUserDetailsService(userDetailsService());
-//        return userDetails;
-//    }
 
 
 }
